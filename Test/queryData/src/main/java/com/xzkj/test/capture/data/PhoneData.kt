01c623901c3e15edcdcbb026b56fbf421ebad67d
wangyuhnag <wangyuhang@xinzhengkeji.com>
2020-10-24 11:25:44 +0800

    PU的JSON格式

--- Test/queryData/src/main/java/com/xzkj/test/capture/data/PhoneData.kt
+++ Test/queryData/src/main/java/com/xzkj/test/capture/data/PhoneData.kt
@@ -2,5 +2,5 @@ package com.xzkj.test.capture.data
 
 import java.io.Serializable
 
-class PhoneData(var phone_text: String = "", var phone_number: String = ""):Serializable {
+class PhoneData(var userPhone: String = "", var userName: String = ""):Serializable {
 }
