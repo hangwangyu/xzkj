3337ab33900da01f3c7323dc86e61b6223b9b588
wangyuhnag <wangyuhang@xinzhengkeji.com>
2020-10-27 13:56:34 +0800

    更新

--- Test/queryData/src/main/java/com/xzkj/test/capture/data/PhoneData.kt
+++ Test/queryData/src/main/java/com/xzkj/test/capture/data/PhoneData.kt
@@ -2,5 +2,5 @@ package com.xzkj.test.capture.data
 
 import java.io.Serializable
 
-class PhoneData(var userPhone: String = "", var userName: String = ""):Serializable {
+class PhoneData( var userName: String = "",var userPhone: String = ""):Serializable {
 }
